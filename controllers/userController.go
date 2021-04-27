caab9bac800c8ecacd06cd84a980da7407d03e3a
chagy <chagy_x_@hotmail.com>
2021-04-27 13:59:16 +0700

    Pagination

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"math"
 	"strconv"
 
 	"../database"
@@ -9,11 +10,28 @@ import (
 )
 
 func AllUsers(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+
+	limit := 1
+
+	offset := (page - 1) * limit
+
+	var total int64
+
 	var users []models.User
 
-	database.DB.Preload("Role").Find(&users)
+	database.DB.Preload("Role").Offset(offset).Limit(limit).Find(&users)
 
-	return c.JSON(users)
+	database.DB.Model(&models.User{}).Count(&total)
+
+	return c.JSON(fiber.Map{
+		"data": users,
+		"meta": fiber.Map{
+			"total":     total,
+			"page":      page,
+			"last_page": math.Ceil(float64(int(total) / limit)),
+		},
+	})
 }
 
 func CreateUser(c *fiber.Ctx) error {
