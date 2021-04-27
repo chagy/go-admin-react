44a4817db1dfe8ef53d4a3dfe0e121d3911a9d79
chagy <chagy_x_@hotmail.com>
2021-04-27 19:35:55 +0700

    Ignore Column

--- models/order.go
+++ models/order.go
@@ -4,9 +4,11 @@ import "gorm.io/gorm"
 
 type Order struct {
 	Id         uint        `json:"id"`
-	FirstName  string      `json:"first_name"`
-	LastName   string      `json:"last_name"`
+	FirstName  string      `json:"-"`
+	LastName   string      `json:"-"`
+	Name       string      `json:"name" gorm:"-"`
 	Email      string      `json:"email"`
+	Total      float32     `json:"total" gorm:"-"`
 	UpdatedAt  string      `json:"updated_at"`
 	CreatedAt  string      `json:"created_at"`
 	OrderItems []OrderItem `json:"order_items" gorm:"foreignKey:OrderId"`
@@ -29,6 +31,17 @@ func (order *Order) Count(db *gorm.DB) int64 {
 
 func (order *Order) Take(db *gorm.DB, limit int, offset int) interface{} {
 	var orders []Order
-	db.Perload("OrderItems").Offset(offset).Limit(limit).Find(&orders)
+	db.Preload("OrderItems").Offset(offset).Limit(limit).Find(&orders)
+
+	for i, _ := range orders {
+
+		var total float32 = 0
+
+		for _, orderItem := range orders[i].OrderItems {
+			total += orderItem.Price * float32(orderItem.Quantity)
+		}
+		orders[i].Name = orders[i].FirstName + " " + orders[i].LastName
+		orders[i].Total = int(total)
+	}
 	return orders
 }
