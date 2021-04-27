1ba4c27856aeb45cac122be9dfcb8bba27eeb57f
chagy <chagy_x_@hotmail.com>
2021-04-27 14:23:48 +0700

    Profile

--- controllers/userController.go
+++ controllers/userController.go
@@ -6,6 +6,7 @@ import (
 
 	"../database"
 	"../models"
+	"../util"
 	"github.com/gofiber/fiber"
 )
 
@@ -86,4 +87,60 @@ func DeleteUser(c *fiber.Ctx) error {
 	database.DB.Delete(&user)
 
 	return nil
+}
+
+func UpdateInfo(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	cookie := c.Cookies("jwt")
+
+	id, _ := util.ParseJwt(cookie)
+
+	userId, _ := strconv.Atoi(id)
+
+	user := models.User{
+		Id:        uint(userId),
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+	}
+
+	database.DB.Model(&user).Updates(user)
+
+	return c.JSON(user)
+}
+
+func UpdatePassword(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	if data["password"] != data["password_confirm"] {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "password do not match",
+		})
+	}
+
+	cookie := c.Cookies("jwt")
+
+	id, _ := util.ParseJwt(cookie)
+
+	userId, _ := strconv.Atoi(id)
+
+	user := models.User{
+		Id: uint(userId),
+	}
+
+	user.SetPassword(data["password"])
+
+	database.DB.Model(&user).Updates(user)
+
+	return c.JSON(user)
 }
